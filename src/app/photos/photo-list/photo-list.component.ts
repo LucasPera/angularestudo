fc066397a0bdcb9f72ac7bcd2db08d52d3f31dbc
LucasPera <lucasperamarchi@hotmail.com>
2020-08-03 13:10:01 -0300

    search componentizado

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo/photo';
 
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { PhotoService } from '../photo/photo.service';
 
 
@@ -12,11 +10,10 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = []; //array do tipo Photo
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,20 +26,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter); //vai registrar depois de 300 milissegundos
+
 	}
   
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe(); //finaliza o debounce quando sai da classe
-  }
-
   load() {
     this.photoService
       .listFromUserPaginate(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = '';
         this.photos = this.photos.concat(photos); //adiciona mais photos na lista
 
         if(!photos.length){
