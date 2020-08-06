a7f92aa53d5b56221fa28079169b880ad3cbe005
LucasPera <lucasperamarchi@hotmail.com>
2020-08-06 00:59:14 -0300

    adicionado login e validação

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -7,8 +10,14 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>; //casca do dom
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
+    ) {}
 
     ngOnInit(): void {
 
@@ -16,5 +25,27 @@ export class SignInComponent implements OnInit {
             userName: ['', Validators.required], //valor padrão
             password: ['', Validators.required] //validação
         }); 
+    }
+
+    login() {
+    
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]), //troca user por userName na url
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+
+                    if(this.platformDetectorService.isPlatformBrowser()) {
+                        this.userNameInput.nativeElement.focus(); //da foco no username 
+                    }
+
+                    alert('Invalid user name or password')
+                }
+            );
     }
 }
